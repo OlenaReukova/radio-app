58e8faa64c82636ab30543551329f2d60e2da728
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-01-03 14:53:46 +0000

    fix: CORS

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: process.env.FRONEND_URL,
-    methods: ["GET"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.options("*", cors());
 
