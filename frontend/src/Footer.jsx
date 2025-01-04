4b18dc7e5117f147143719bc879b5c4b34571d78
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-01-04 13:58:17 +0000

    Update Footer.jsx

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-left">
-        <p> 2024</p>
+        <p> 2024-2025</p>
       </div>
       <div className="footer-right"></div>
     </footer>
