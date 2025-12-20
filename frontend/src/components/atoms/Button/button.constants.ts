68949f23b6ecd8e2918aaa4ef6815d799d83e5ce
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2025-12-20 13:05:52 +0000

    feat:add button component

--- frontend/src/components/atoms/Button/button.constants.ts
+++ frontend/src/components/atoms/Button/button.constants.ts
@@ -1,1 +1,16 @@
-//optional enums/constants
+//1. optional enums/constants
+
+export const BUTTON_VARIANTS = {
+  PRIMARY: "primary",
+  SECONDARY: "secondary",
+  OUTLINE: "outline",
+  GHOST: "ghost",
+  ICON: "icon",
+  DESTRUCTIVE: "destructive",
+} as const;
+
+export const BUTTON_SIZE = {
+  SM: "sm",
+  MD: "md",
+  LG: "lg",
+} as const;
--- frontend/src/components/atoms/Button/button.types.ts
+++ frontend/src/components/atoms/Button/button.types.ts
@@ -1,1 +1,10 @@
-//TypeScript API
+//3. TypeScript/public API
+import type { ButtonHTMLAttributes } from "react";
+import type { VariantProps } from "class-variance-authority";
+import { buttonVariants } from "./button.variants";
+
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  loading?: boolean;
+}
