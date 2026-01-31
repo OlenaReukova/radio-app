bda90763d01f6f9312517322bf5819190fbe6eb5
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2026-01-31 18:58:36 +0000

    fix:errors

--- frontend/src/components/molecules/SearchInput/SearchInput.tsx
+++ frontend/src/components/molecules/SearchInput/SearchInput.tsx
@@ -1,11 +1,32 @@
 import * as React from "react";
 import { Input } from "../../atoms/Input";
 
-export interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export type SearchInputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const SearchInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
-  (props, ref) => {
-    return <Input ref={ref} shape="pill" {...props} />;
+  ({ value: controlledValue, onChange, ...props }, ref) => {
+    const [internalValue, setInternalValue] = React.useState(
+      controlledValue ?? "",
+    );
+
+    React.useEffect(() => {
+      if (controlledValue !== undefined) {
+        setInternalValue(controlledValue);
+      }
+    }, [controlledValue]);
+
+    return (
+      <Input
+        ref={ref}
+        shape="pill"
+        value={internalValue}
+        onChange={(e) => {
+          setInternalValue(e.target.value);
+          onChange?.(e);
+        }}
+        {...props}
+      />
+    );
   },
 );
 
