308d815b80c3d93af6a4a08227ac1723eec72085
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2026-02-04 11:45:05 +0000

    move sidebar

--- frontend/src/components/atoms/Sidebar/sidebar.tsx
+++ frontend/src/components/atoms/Sidebar/sidebar.tsx
@@ -3,25 +3,25 @@ import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import { PanelLeftIcon } from "lucide-react";
 
-import { useIsMobile } from "./use-mobile";
-import { cn } from "./utils";
-import { Button } from "./button";
-import { Input } from "./input";
-import { Separator } from "./separator";
+import { useIsMobile } from "../../ui/use-mobile";
+import { cn } from "../../ui/utils";
+import { Button } from "../Button";
+import { Input } from "../../ui/input";
+import { Separator } from "../../ui/separator";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
-} from "./sheet";
-import { Skeleton } from "./skeleton";
+} from "../../ui/sheet";
+import { Skeleton } from "../../ui/skeleton";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "../atoms/Tooltip/tooltip";
+} from "../Tooltip/tooltip";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
